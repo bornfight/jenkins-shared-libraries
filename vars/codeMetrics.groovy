1af09757015711b589296dcf0c719d3f911106f0
JosipTrbuscic <josip.trbuscic@bornfight.com>
2019-06-18 13:43:46 +0200

    Remove duplicate file

--- vars/codeMetrics.groovy
+++ vars/codeMetrics.groovy
@@ -2,7 +2,7 @@ def call(String repoName) {
     checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'code-metrics-observatory']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'istudio', url: 'git@github.com:degordian/code-metrics-observatory.git']]])
     dir('code-metrics-observatory') {
         sh 'composer install'
-        configFileProvider([configFile(fileId: 'c3d7339a-64c3-43ed-a9f0-51a2423f267c', targetLocation: '.env.local'), configFile(fileId: 'c3d7339a-64c3-43ed-a9f0-51a2423f267c', targetLocation: '.env.test.local')]) {
+        configFileProvider([configFile(fileId: 'c3d7339a-64c3-43ed-a9f0-51a2423f267c', targetLocation: '.env.test.local')]) {
             sh "bin/console collect ${repoName} ../tests/_output/report.xml"
         }
     }
