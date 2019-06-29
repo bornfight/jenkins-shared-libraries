6dd3fdc52cde8b22ea642290575696b1332f7703
JosipTrbuscic <josip.trbuscic@bornfight.com>
2019-06-29 12:55:55 +0200

    Test post block

--- vars/deployEnvironment.groovy
+++ vars/deployEnvironment.groovy
@@ -1,7 +1,22 @@
 def call(String env, String branch, String tag, String[][] configs, String sshAgentId) {
+    env = isUndefined(env) ? "integration" : env
+
+    if(env == "integration" && branch != "master") return
+
+    if(!isUndefined(tag) && env == "integration"){
+        currentBuild.result = 'ABORTED'
+        error('Aborting first tag build due to undefined stage')
+    }
+
+
     echo env
     echo branch
     echo tag
     echo configs[0][0]
     echo sshAgentId
+    currentBuild.result = 'ABORTED'
+}
+
+static boolean isUndefined(String var){
+    return var == null || var.trim().isEmpty() || var.trim() == "null"
 }
