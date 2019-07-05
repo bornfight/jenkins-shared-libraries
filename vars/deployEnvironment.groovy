fcc57966951fb1f016285fdc1076536cc6e257ad
JosipTrbuscic <josip.trbuscic@bornfight.com>
2019-07-05 13:34:57 +0200

    Formatting

--- vars/deployEnvironment.groovy
+++ vars/deployEnvironment.groovy
@@ -1,7 +1,7 @@
 def call(String env, String[][] configs, String[] sshAgentIds) {
     env = isUndefined(env) ? "integration" : env
 
-    String configArgument = prepareConfigProviderArguments()
+    String configArgument = prepareConfigProviderArguments(configs)
     echo configArgument
 
     String sshAgents = prepareSshAgentArguments(sshAgentIds)
@@ -15,29 +15,29 @@ def call(String env, String[][] configs, String[] sshAgentIds) {
     }
 }
 
-static boolean isUndefined(String var){
+static boolean isUndefined(String var) {
     return var == null || var.trim().isEmpty() || var.trim() == "null"
 }
 
-static String prepareConfigProviderArguments(String[][] configs){
+static String prepareConfigProviderArguments(String[][] configs) {
     StringBuilder arguments = new StringBuilder();
 
-    for(int i = 0; i < configs.length; i++){
+    for (int i = 0; i < configs.length; i++) {
         arguments.append("configFile(fileId: '" + configs[i][0] + "', targetLocation: '" + configs[i][1] + "')")
-        if(i < configs.length -1){
-          arguments.append(", ")
+        if (i < configs.length - 1) {
+            arguments.append(", ")
         }
     }
 
     return arguments.toString();
 }
 
-static String prepareSshAgentArguments(String[] sshAgentIds){
+static String prepareSshAgentArguments(String[] sshAgentIds) {
     StringBuilder arguments = new StringBuilder()
 
-    for(int i = 0; i < sshAgentIds.length; i++){
+    for (int i = 0; i < sshAgentIds.length; i++) {
         arguments.append("'" + sshAgentIds[i] + "'")
-        if(i < sshAgentIds.length -1){
+        if (i < sshAgentIds.length - 1) {
             arguments.append(", ")
         }
     }
