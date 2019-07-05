2fd3465524dfa632001a1e6fd7d724168d15e16e
JosipTrbuscic <josip.trbuscic@bornfight.com>
2019-07-05 13:31:32 +0200

    Prepeare configs and ssh agents as strings

--- vars/deployEnvironment.groovy
+++ vars/deployEnvironment.groovy
@@ -1,25 +1,46 @@
-def call(String env, String branch, String tag, String[][] configs, String sshAgentId) {
+def call(String env, String[][] configs, String[] sshAgentIds) {
     env = isUndefined(env) ? "integration" : env
 
-    if(env == "integration" && branch != "master"){
-        currentBuild.result = "SKIP"
-        return
+    String configArgument = prepareConfigProviderArguments()
+    echo configArgument
+
+    String sshAgents = prepareSshAgentArguments(sshAgentIds)
+    echo sshAgents
+
+
+    configFileProvider([prepareConfigProviderArguments(configs)]) {
+        sshagent([prepareSshAgentArguments(sshAgentIds)]) {
+            echo "Deploy ${env}"
+        }
     }
-
-    if(!isUndefined(tag) && env == "integration"){
-        currentBuild.result = "ABORTED"
-        return
-    }
-
-
-    echo env
-    echo branch
-    echo tag
-    echo configs[0][0]
-    echo sshAgentId
-    currentBuild.result = "SKIP"
 }
 
 static boolean isUndefined(String var){
     return var == null || var.trim().isEmpty() || var.trim() == "null"
+}
+
+static String prepareConfigProviderArguments(String[][] configs){
+    StringBuilder arguments = new StringBuilder();
+
+    for(int i = 0; i < configs.length; i++){
+        arguments.append("configFile(fileId: '" + configs[i][0] + "', targetLocation: '" + configs[i][1] + "')")
+        if(i < configs.length -1){
+          arguments.append(", ")
+        }
+    }
+
+    return arguments.toString();
+}
+
+static String prepareSshAgentArguments(String[] sshAgentIds){
+    StringBuilder arguments = new StringBuilder()
+
+    for(int i = 0; i < sshAgentIds.length; i++){
+        arguments.append("'" + sshAgentIds[i] + "'")
+        if(i < sshAgentIds.length -1){
+            arguments.append(", ")
+        }
+    }
+
+    return arguments.toString()
 }
