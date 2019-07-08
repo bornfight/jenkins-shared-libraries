901c7fb6f5fa91f7f1acf5bd09b8d64c443154ff
JosipTrbuscic <josip.trbuscic@bornfight.com>
2019-07-08 13:49:44 +0200

    Revert debug change

--- vars/setDeployEnv.groovy
+++ vars/setDeployEnv.groovy
@@ -1,21 +1,20 @@
 def call(String environment) {
     int buildNumber = Integer.parseInt("${BUILD_NUMBER}")
     String buildTag
+
     try {
         buildTag = "${TAG_NAME}"
     } catch (MissingPropertyException ignored) {
-        echo "b tag catch: ${buildTag}"
         buildTag = ""
     }
 
     environment = isUndefined(environment) || isUndefined(buildTag) ? "integration" : environment
-    echo "b tag: ${buildTag}"
 
     if (buildNumber == 1 && !isUndefined(buildTag)) {
         environment = "staging"
     }
-    echo "end env: ${environment}"
-    return "st"
+
+    return environment
 }
 
 static boolean isUndefined(String var) {
