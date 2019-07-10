8d77cf92211c5e2bd770f3552cd0ee711ef312a3
JosipTrbuscic <josip.trbuscic@bornfight.com>
2019-07-10 17:01:36 +0200

    Fix imports

--- src/com/bornfight/DeployedTagsTracker.groovy
+++ src/com/bornfight/DeployedTagsTracker.groovy
@@ -6,6 +6,8 @@ package com.bornfight
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.sheets.v4.Sheets
 import com.google.api.services.sheets.v4.SheetsScopes
 import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
