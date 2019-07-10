354a3fba164e5141cbd2814f4b3074bffe492f9e
JosipTrbuscic <josip.trbuscic@bornfight.com>
2019-07-10 18:50:33 +0200

    Add import

--- src/com/bornfight/DeployedTagsTracker.groovy
+++ src/com/bornfight/DeployedTagsTracker.groovy
@@ -3,6 +3,7 @@ package com.bornfight
 @Grab(group = 'com.google.api-client', module = 'google-api-client', version = '1.30.1')
 @Grab(group = 'com.google.apis', module = 'google-api-services-sheets', version = 'v4-rev581-1.25.0')
 @Grab(group = 'com.google.oauth-client', module = 'google-oauth-client-jetty', version = '1.23.0')
+import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.client.http.javanet.NetHttpTransport
