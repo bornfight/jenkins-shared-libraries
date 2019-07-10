2b37bd545c8cd23c228fe239d6a76ab82e819dd7
JosipTrbuscic <josip.trbuscic@bornfight.com>
2019-07-10 17:22:02 +0200

    Fix credentials loading

--- src/com/bornfight/DeployedTagsTracker.groovy
+++ src/com/bornfight/DeployedTagsTracker.groovy
@@ -15,20 +15,20 @@ import com.google.api.services.sheets.v4.model.ValueRange;
 
 class DeployedTagsTracker implements Serializable{
 
-    private static final String APPLICATION_NAME = "DeployedTagsTracker";
-    private static final String CREDENTIALS_FILE_PATH = "credentials.json";
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private final String APPLICATION_NAME = "DeployedTagsTracker";
+    private final String CREDENTIALS_FILE_PATH = "credentials.json";
+    private final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
-    private static def getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+    private def getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Load client secrets.
-        InputStream is = DeployedTagsTracker.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        InputStream is = new FileInputStream(CREDENTIALS_FILE_PATH);
         if (is == null) {
             throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
         }
         return GoogleCredential.fromStream(is).createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS));
     }
 
-    static def update(String sheetId, String project, String stage, String tag){
+    def update(String sheetId, String project, String stage, String tag){
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
         final String range = "DeploymentTracker!A2:C";
         Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
